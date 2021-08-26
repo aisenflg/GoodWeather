501aab2683fc96d709075a565acce32b2eeed3d4
K <467145776@qq.com>
2021-08-26 18:02:27 +0800

    添加国内热门城市以及常用城市功能

---
+++ app/src/main/java/com/example/goodweather/adapter/CommonlyCityAdapter.java
@@ -0,0 +1,2 @@
+package com.example.goodweather.adapter;public class CommonlyCityAdapter {
+}
---
+++ app/src/main/java/com/example/goodweather/ui/CommonlyUsedCityActivity.java
@@ -0,0 +1,14 @@
+package com.example.goodweather.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommonlyUsedCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commonly_used_city);
+    }
+}
